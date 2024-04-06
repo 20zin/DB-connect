9b9d435538ceecbe404f742a1deea0f0a6f232e2
YoungjinLee <117518123+20zin@users.noreply.github.com>
2024-04-06 16:40:10 +0900

    7 commit

--- src/main/java/hello/itemservice/config/SpringDataJpaConfig.java
+++ src/main/java/hello/itemservice/config/SpringDataJpaConfig.java
@@ -1,22 +1,23 @@
 package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepositoryV2;
+import hello.itemservice.repository.jpa.SpringDataJpaItemRepository;
 import hello.itemservice.repository.mybatis.MybatisItemRepository;
 import hello.itemservice.service.ItemService;
 import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
 
 @Configuration
-public class JpaConfig {
+@RequiredArgsConstructor
+public class SpringDataJpaConfig {
 
-    private final EntityManager em;
+    private final SpringDataJpaItemRepository springDataJpaItemRepository;
 
-    public JpaConfig(EntityManager em) {
-        this.em = em;
-    }
     @Bean
     public ItemService itemService(){
         return new ItemServiceV1(itemRepository());
@@ -24,7 +25,7 @@ public class JpaConfig {
 
     @Bean
     public ItemRepository itemRepository() {
-        return new MybatisItemRepository(em);
+        return new JpaItemRepositoryV2(springDataJpaItemRepository);
     }
 
 }
